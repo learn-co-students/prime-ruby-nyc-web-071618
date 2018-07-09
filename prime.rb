fb9196d1605de00dbd6fd50c98b0f6c8042bc1ac
cmonkey03 <russellpbrown@gmail.com>
2018-07-08 20:49:24 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+def prime?(number)
+  i = 1
+  divisible_numbers = 1
+  while i < number
+    if number % i == 0
+      divisible_numbers +=1
+    end
+    i += 1
+  end
+  if number > 1
+    if divisible_numbers > 2
+      false
+    else
+      true
+    end
+  else
+    false
+  end
+end
