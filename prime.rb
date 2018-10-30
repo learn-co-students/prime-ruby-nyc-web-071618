a96309b11ff06042613dbab7a39b55f76a88e459
dagonyclark <sean.dagony-clark@flatironschool.com>
2018-10-29 22:17:30 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,37 +1,33 @@
-def prime?(number)
-  # half = number * .5
-  if number < 2
-    # puts "No negatives and your number must be > 1"
-    return false
-  end
-
-  array = 1..number
+def prime?(test)
+  iterator = test / 2
+  counter = 1..iterator
   divisors = 0
-  counter = 1
 
-  while divisors <= 2 && counter <= 0.5 * number
-    if number % counter == 0 # has a divisor
-      divisors += 1
-      # puts "#{divisors} divisors"
-    end
-    counter += 1
-  end
-
-  if divisors >= 2
-    # puts "Your number (#{number}) is not prime."
-    return false
-  else
-    # puts "Your number (#{number}) is prime!"
+  if test == 2
+    puts "#{test} has #{divisors} divisors. It's Optimus Prime!"
     return true
+  elsif test > 1
+    for iteration in counter
+      puts "Testing #{test} / #{iteration}"
+      if test % iteration == 0
+        divisors += 1
+        if divisors > 1
+          puts "#{test} is not prime, sucker."
+          return false
+        end
+        puts "Counted #{divisors} divisors in #{test}."
+      end
+    end
+    if divisors == 1
+      puts "#{test} has #{divisors + 1} divisors. It's Optimus Prime!"
+      return true
+    end
+  else
+    puts "must be greater than 1!"
+    return false
   end
+
 
 end
 
-# puts "Enter a number and I'll tell you whether it's prime!"
-# prime?(-1)
-# prime?(0)
-# prime?(1)
-# prime?(4)
-# prime?(40)
-# prime?(1763)
-# prime?(101013)
+# prime?(172)
